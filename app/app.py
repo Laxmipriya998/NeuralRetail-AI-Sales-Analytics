ae15f2bac20d17ec999ea5a2eeeee0e8f8e9a605
Laxmipriya998 <laxmip9sahoo@gmail.com>
2023-02-09 11:00:00 +0530

    Added final Streamlit dashboard with KPI cards and charts

--- app/app.py
+++ app/app.py
@@ -1,88 +1,140 @@
-import requests
 import streamlit as st
-
 import pandas as pd
+import os
 
-import matplotlib.pyplot as plt
-
-import seaborn as sns
-
-def get_demand():
-    return requests.get(
-        "http://127.0.0.1:8000/predict/demand"
-    ).json()
-
-def get_churn():
-    return requests.get(
-        "http://127.0.0.1:8000/predict/churn"
-    ).json()
-    
-st.title("NeuralRetail AI Dashboard")
-
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.abspath(__file__)
+    )
+)
 
 sales_df = pd.read_csv(
-    "../data/processed/cleaned_retail_data.csv"
+    os.path.join(BASE_DIR, "data", "processed", "cleaned_retail_data.csv")
 )
 
 forecast_df = pd.read_csv(
-    "../data/processed/forecast_data.csv"
+    os.path.join(BASE_DIR, "data", "processed", "forecast_data.csv")
 )
 
+rfm_df = pd.read_csv(
+    os.path.join(BASE_DIR, "data", "processed", "rfm_customer_segments.csv")
+)
+
+inventory_df = pd.read_csv(
+    os.path.join(BASE_DIR, "data", "processed", "inventory_data.csv")
+)
+
+st.title("NeuralRetail AI Dashboard")
 
 page = st.sidebar.selectbox(
-
     "Select Dashboard",
-
     [
+        "Executive Overview",
         "Sales Dashboard",
-        "Forecast Dashboard"
+        "Forecast Dashboard",
+        "Customer Dashboard",
+        "Inventory Dashboard"
     ]
-
 )
 
+if page == "Executive Overview":
 
-if page == "Sales Dashboard":
-
-    st.header("Sales Dashboard")
+    st.header("Executive Overview")
 
     total_sales = sales_df["TotalSales"].sum()
+    total_customers = sales_df["Customer ID"].nunique()
+    total_orders = sales_df["Invoice"].nunique()
+    avg_order_value = total_sales / total_orders
 
-    st.metric(
-        "Total Sales",
-        f"${total_sales:,.2f}"
-    )
+    col1, col2, col3, col4 = st.columns(4)
+
+    col1.metric("Total Sales", f"${total_sales:,.0f}")
+    col2.metric("Customers", total_customers)
+    col3.metric("Orders", total_orders)
+    col4.metric("Avg Order Value", f"${avg_order_value:,.2f}")
+
+
+
+elif page == "Sales Dashboard":
+
+    st.header("Sales Dashboard")
 
     monthly_sales = sales_df.groupby(
         "Month"
     )["TotalSales"].sum()
 
-    fig, ax = plt.subplots(figsize=(10,5))
+    st.subheader("Monthly Sales Trend")
+    st.line_chart(monthly_sales)
 
-    monthly_sales.plot(ax=ax)
+    country_sales = sales_df.groupby(
+        "Country"
+    )["TotalSales"].sum().sort_values(
+        ascending=False
+    ).head(10)
 
-    plt.title("Monthly Sales")
+    st.subheader("Top 10 Countries by Sales")
+    st.bar_chart(country_sales)
 
-    st.pyplot(fig)
 
 
 elif page == "Forecast Dashboard":
 
     st.header("Forecast Dashboard")
 
-    fig, ax = plt.subplots(figsize=(12,5))
+    st.subheader("Actual vs Predicted Sales")
 
-    ax.plot(
-        forecast_df["Date"],
-        forecast_df["Sales"],
-        label="Actual Sales"
-    )
+    forecast_chart = forecast_df[
+        ["Sales", "PredictedSales"]
+    ]
 
-    ax.plot(
-        forecast_df["Date"],
-        forecast_df["PredictedSales"],
-        label="Predicted Sales"
-    )
+    st.line_chart(forecast_chart)
 
-    plt.legend()
 
-    st.pyplot(fig)
+
+elif page == "Customer Dashboard":
+
+    st.header("Customer Dashboard")
+
+    st.subheader("RFM Score Distribution")
+
+    rfm_counts = rfm_df[
+        "RFM_Score"
+    ].value_counts()
+
+    st.bar_chart(rfm_counts)
+
+    st.subheader("Top 10 Customers")
+
+    top_customers = rfm_df.sort_values(
+        "Monetary",
+        ascending=False
+    ).head(10)
+
+    st.dataframe(top_customers)
+
+
+
+elif page == "Inventory Dashboard":
+
+    st.header("Inventory Dashboard")
+
+    st.subheader("Top Products by Revenue")
+
+    top_products = inventory_df.sort_values(
+        "TotalRevenue",
+        ascending=False
+    ).head(10)
+
+    chart_data = top_products.set_index(
+        "Product"
+    )["TotalRevenue"]
+
+    st.bar_chart(chart_data)
+
+    st.subheader("Low Stock Products")
+
+    low_stock = inventory_df[
+        inventory_df["LowStockAlert"] == 1
+    ]
+
+    st.dataframe(low_stock)
